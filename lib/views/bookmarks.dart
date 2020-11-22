00e042dd91b22d616c20abea7116a447c6a366b9
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-11-22 12:50:38 +0000

    add bottom nav bar

---
+++ lib/views/bookmarks.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Bookmarks extends StatefulWidget {
+  _BookmarksState createState() => _BookmarksState();
+}
+
+class _BookmarksState extends State<Bookmarks> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Bookmarks'),
+    );
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
-import 'package:stoic/widgets/appbar.dart';
+// import 'package:stoic/widgets/appbar.dart';
+import 'package:stoic/views/bookmarks.dart';
+import 'package:stoic/views/settings.dart';
 
 class MyHome extends StatefulWidget {
   _MyHomeState createState() => _MyHomeState();
 }
 
 class _MyHomeState extends State<MyHome> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final Map<int, Widget> _views = {0: Bookmarks(), 1: Settings()};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar,
-      body: null,
-      // bottomNavigationBar: BottomNavigationBar(),
+      // appBar: MyAppBar,
+      body: _views[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark), label: 'Bookmarks'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.cyan,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
