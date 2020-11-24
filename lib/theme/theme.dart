c1e0065598ec4f510dc0f7c7291289dab8e3b95c
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-11-24 07:35:43 +0000

    move theme from app.dart to a dedicaded file

---
+++ lib/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:stoic/theme/text.dart';
+
+ThemeData myTheme = ThemeData(
+  primaryColor: Color(0xff4ecdc4),
+  accentColor: Color(0xff4ecdc4),
+  scaffoldBackgroundColor: Color(0xfffdfdfd),
+  buttonColor: Color(0xff555b69),
+  textTheme: myTextTheme,
+);
--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:stoic/views/home.dart';
-import 'package:stoic/theme/text.dart';
+import 'package:stoic/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,17 +9,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MyApp',
-      // home: MyHome(),
       initialRoute: '/',
       routes: {
         '/': (context) => MyHome(),
       },
-      theme: ThemeData(
-          primaryColor: Color(0xff4ecdc4),
-          accentColor: Color(0xff4ecdc4),
-          scaffoldBackgroundColor: Color(0xfffdfdfd),
-          buttonColor: Color(0xff555b69),
-          textTheme: myTextTheme),
+      theme: myTheme,
     );
   }
 }
