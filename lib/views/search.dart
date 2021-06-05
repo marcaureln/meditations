fcb7e6ce115227cba03acb4e0e036d04824aecb8
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-06-05 13:26:13 +0000

    using search delegate

---
+++ lib/views/search.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:stoic/models/quote.dart';
+import 'package:stoic/theme/text.dart';
+
+class QuoteSearchDelegate extends SearchDelegate<Quote> {
+  final List<Quote> quotes;
+
+  QuoteSearchDelegate(this.quotes)
+      : super(
+          searchFieldLabel: 'Search...',
+          searchFieldStyle: myTextTheme.bodyText1,
+        );
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<Quote> searchResults = _search(query, quotes);
+
+    return ListView.separated(
+      padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 128),
+      itemCount: searchResults.length,
+      itemBuilder: (context, index) {
+        Quote quote = searchResults[index];
+
+        return Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Container(width: double.infinity, child: Text('"${quote.content}"')),
+              Container(width: double.infinity, child: Text((quote.author != null) ? quote.author : 'anonymous')),
+              if (quote.source != null) Container(width: double.infinity, child: Text(quote.source)),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, _) => const Divider(height: 12),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<Quote> searchResults = _search(query, quotes);
+
+    return ListView.separated(
+      padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 128),
+      itemCount: searchResults.length,
+      itemBuilder: (context, index) {
+        Quote quote = searchResults[index];
+
+        return Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Container(width: double.infinity, child: Text('"${quote.content}"')),
+              Container(width: double.infinity, child: Text((quote.author != null) ? quote.author : 'anonymous')),
+              if (quote.source != null) Container(width: double.infinity, child: Text(quote.source)),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, _) => const Divider(height: 12),
+    );
+  }
+
+  List<Quote> _search(String terms, List<Quote> list) {
+    return list
+        .where((quote) =>
+            quote.content.toLowerCase().contains(terms) ||
+            (quote.author?.toLowerCase()?.contains(terms) ?? false) ||
+            (quote.source?.toLowerCase()?.contains(terms) ?? false))
+        .toList();
+  }
+}
